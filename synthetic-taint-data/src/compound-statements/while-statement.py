862640c4fb4232d5636360e99c9522f50329812a
Rajiv Thorat <rajiv.thorat@codeshield.io>
2022-03-22 16:49:24 +0100

    Completed the while test

--- synthetic-taint-data/src/compound-statements/while-statement.py
+++ synthetic-taint-data/src/compound-statements/while-statement.py
@@ -5,15 +5,22 @@ from secrets import randbelow
 
 class while_loop:
     def randomize_prime(self):
-        random_number = randint(0,20)
-        while not self.is_prime(random_number):
-            print("the number is prime. reading the character sheet.")
-            return json.load(open(abspath("synthetic-taint-data/resources/character-sheet.json")))
-        print("The randomly generated number was not a prime number")
+        random_number = randint(1,20)
+        max_run_count = 10
+        while not self.is_prime(random_number) and max_run_count != 0:
+            print("the number is not prime. reading the character sheet.")
+            character_sheet = json.load(open(abspath("synthetic-taint-data/resources/character-sheet.json")))
+            print(character_sheet["race"])
+            random_number = randint(0, 20)
+            max_run_count = max_run_count - 1
+        print("The randomly generated number was a prime number")
     def is_prime(self, random_number):
-        for i in range(2, random_number):
+        if random_number == 2 or random_number == 3:
+            return True
+        for i in range(2, random_number - 1):
             if random_number % i == 0:
                 return False
+        return True
 
 if __name__ == "__main__":
     while_loop_ = while_loop()
