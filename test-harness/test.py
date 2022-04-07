ca239364c0251eb02320b6257e1effacd56bebf0
Rajiv Thorat <rajiv.thorat@codeshield.io>
2022-04-07 17:39:48 +0200

    Updated the tests

--- test-harness/test.py
+++ test-harness/test.py
@@ -1,6 +1,8 @@
+from dataclasses import dataclass
 import json
 import utils
 
 data = open("/Users/rajivthorat/Code/MasterArbeit/submodules/benchmark-for-taint-analysis-tools-for-python/tests/synthetic-taint-data/src/compound-statements/for-statement/snyk-result.json", "r")
-data_clean = utils.clean_stdout()
+data_clean = utils.clean_stdout(data.read())
+json_data = json.loads(data_clean)
 utils.write_to_file("test.json", data)
