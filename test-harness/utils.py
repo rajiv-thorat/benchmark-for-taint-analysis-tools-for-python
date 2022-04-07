a39463c6a7aec0adc77e907a72c2e5696da8e8a5
Rajiv Thorat <rajiv.thorat@codeshield.io>
2022-04-07 17:39:14 +0200

    Updated the utils file for cleaning stdout

--- test-harness/utils.py
+++ test-harness/utils.py
@@ -1,9 +1,9 @@
-from black import NewLine
+import os
 
-
+# This is for the output from the Snyk tool. The result is enclosed in single quotes, and there is a "b"
+# at the begining that need to be removed for the string to be parsable as JSON.
 def clean_stdout(stdout_raw:str):
-    print(stdout_raw)
-    return stdout_raw.replace("\n", NewLine)
+    return stdout_raw[2:len(stdout_raw) - 1].replace("\\n", os.linesep)
 
 def write_to_file(file_name:str, data_to_write:str):
     result_file = open(file_name, "w")
