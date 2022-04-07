05e446126733efaae1e8d099cce127049a136fdd
Rajiv Thorat <rajiv.thorat@codeshield.io>
2022-04-07 19:02:27 +0200

    Temp changes to test

--- test-harness/snyk_harness.py
+++ test-harness/snyk_harness.py
@@ -2,27 +2,31 @@ import json
 import os
 import subprocess
 import utils
+import logging
 
 
-def run_benchmark_on_snyk(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS, DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA):
-    for file in os.listdir(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS):
-        project_root = DIRECTORY_PATH_FOR_REALWORLD_PROJECTS + file
-        if(os.path.isdir(project_root)):
-            run_snyk(project_root)
+def run_benchmark_on_snyk(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS, DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA, DIRECTORY_PATH_FOR_TESTS):
+    #for file in os.listdir(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS):
+    #    project_root = os.path.join(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS, file)
+    #    if(os.path.isdir(project_root)):
+    #        run_snyk(project_root)
+    #logging.info("Finished running Snyk Code on the real world projects.")
 
-    for file in os.listdir(DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA):
-        project_root = os.path.join(DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA, file)
-        if(os.path.isdir(project_root) and file != ".DS_Store" and file!= "__pycache__"):
-            run_snyk(project_root)
+    #for file in os.listdir(DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA):
+    #    project_root = os.path.join(DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA, file)
+    #    if(os.path.isdir(project_root) and file != ".DS_Store" and file!= "__pycache__"):
+    #        run_snyk(project_root)
+    #logging.info("Finished running Snyk Code on the synthetic tests.")
+    run_snyk(DIRECTORY_PATH_FOR_TESTS)
 
 def run_snyk(project_root):
-    print(f"Running Snyk code in {project_root}")
+    logging.info(f"Running Snyk code in {project_root}")
     #command = f"snyk code test --json {project_root}"
     result_file_location = os.path.join(project_root, "snyk-result.json")
     # for some reason the --json-output-file does not generate the target file. 
     # Reading the stdout and cleaning it instead
     command = ["snyk", "code", "test", "--json", project_root]
-    print(command)
+    logging.debug(command)
     command_output = subprocess.run(command, capture_output=True)
     command_output.check_returncode()
     utils.write_to_file(os.path.join(project_root, "snyk-result.json"), utils.clean_stdout(str(command_output.stdout)))
--- test-harness/test_harness.py
+++ test-harness/test_harness.py
@@ -1,12 +1,22 @@
+from distutils.debug import DEBUG
 from posixpath import abspath
 import snyk_harness
+import logging
 
+
+logging.basicConfig(level=DEBUG, format="%(asctime)s %(levelname)s:%(message)s")
+logging.info("Starting the benchmark.")
 DIRECTORY_PATH_FOR_REALWORLD_PROJECTS = abspath("../tests/real-world-projects/")
+logging.debug(f"The realworld projects are at {DIRECTORY_PATH_FOR_REALWORLD_PROJECTS}")
 # The synthetic tests directory may contain subdirectories.
 # The subdirectories will contain roots for individual projects (tests)
 DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA = abspath(
     "../tests/synthetic-taint-data/src/compound-statements/")
+logging.debug(f"The synthetic tests are are at {DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA}")
+DIRECTORY_PATH_FOR_TESTS = abspath("../tests/")
+logging.debug(f"All the tests are at {DIRECTORY_PATH_FOR_TESTS}")
 
-snyk_harness.run_benchmark_on_snyk(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS, DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA)
-
+logging.info("Running Snyk Code on the benchmark.")
+snyk_harness.run_benchmark_on_snyk(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS, DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA, DIRECTORY_PATH_FOR_TESTS)
+logging.info("Finished running the benchmark using Snyk code.")
 
