22a11f48794dc48015731eb63413bea4dca3eef9
Rajiv Thorat <rajiv.thorat@codeshield.io>
2022-04-14 17:53:06 +0200

    Running snyk over each project

--- test-harness/snyk_harness.py
+++ test-harness/snyk_harness.py
@@ -10,24 +10,24 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 def run_benchmark_on_snyk(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS, DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA, DIRECTORY_PATH_FOR_TESTS):
-    # for file in os.listdir(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS):
-    #    project_root = os.path.join(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS, file)
-    #    if(os.path.isdir(project_root)):
-    #        run_snyk(project_root)
-    #logging.info("Finished running Snyk Code on the real world projects.")
+    for file in os.listdir(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS):
+       project_root = os.path.join(DIRECTORY_PATH_FOR_REALWORLD_PROJECTS, file)
+       if(os.path.isdir(project_root)):
+           run_snyk(project_root)
+    logging.info("Finished running Snyk Code on the real world projects.")
 
-    #for file in os.listdir(DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA):
-    #    project_root = os.path.join(DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA, file)
-    #    if(os.path.isdir(project_root) and file != ".DS_Store" and file!= "__pycache__"):
-    #        run_snyk(project_root)
-    #logging.info("Finished running Snyk Code on the synthetic tests.")
+    for file in os.listdir(DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA):
+       project_root = os.path.join(DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA, file)
+       if(os.path.isdir(project_root) and file != ".DS_Store" and file!= "__pycache__"):
+           run_snyk(project_root)
+    logging.info("Finished running Snyk Code on the synthetic tests.")
     # run_snyk(DIRECTORY_PATH_FOR_TESTS)
-    logging.info("Finished running snyk on the project.")
+    # logging.info("Finished running snyk on the project.")
 
 def run_snyk(project_root):
     logging.info(f"Running Snyk code in {project_root}")
     #command = f"snyk code test --json {project_root}"
-    result_file_location = os.path.join(project_root, "snyk-result__.json")
+    result_file_location = os.path.join(project_root, "snyk-result.json")
     # for some reason the --json-output-file does not generate the target file. 
     # Reading the stdout and cleaning it instead
     command = ["snyk", "code", "test", "--json", project_root]
--- test-harness/test_harness.py
+++ test-harness/test_harness.py
@@ -6,12 +6,12 @@ import logging
 logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 logging.info("Starting the benchmark.")
-DIRECTORY_PATH_FOR_REALWORLD_PROJECTS = abspath("../tests/real-world-projects/")
+DIRECTORY_PATH_FOR_REALWORLD_PROJECTS = abspath("tests/real-world-projects/")
 logging.debug(f"The realworld projects are at {DIRECTORY_PATH_FOR_REALWORLD_PROJECTS}")
 # The synthetic tests directory may contain subdirectories.
 # The subdirectories will contain roots for individual projects (tests)
 DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA = abspath(
-    "../tests/synthetic-taint-data/src/compound-statements/")
+    "tests/synthetic-taint-data/src/compound-statements/")
 logging.debug(f"The synthetic tests are are at {DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA}")
 DIRECTORY_PATH_FOR_TESTS = abspath("tests/")
 logging.debug(f"All the tests are at {DIRECTORY_PATH_FOR_TESTS}")
