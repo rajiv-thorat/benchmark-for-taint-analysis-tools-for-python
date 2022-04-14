b77d64340b0eb7a3932bf47bbf71316fc9e4ff2d
Rajiv Thorat <rajiv.thorat@codeshield.io>
2022-04-14 18:06:27 +0200

    Moved the synthetic test projects
    Fixed the for test

--- tests/synthetic-taint-data/for-statement/for-statement.py
+++ tests/synthetic-taint-data/for-statement/for-statement.py
@@ -4,7 +4,7 @@ import json
 from os.path import abspath
 
 class for_loop:
-   def find_character_sheet(self, path):
+    def find_character_sheet(self, path):
        character_sheet = None
        for file in os.listdir(path):
            complete_path = os.path.join(path, file)
@@ -18,10 +18,10 @@ class for_loop:
        if character_sheet is None:
             print("There was a problem reading the file.")
 
-def print_character(self, character_sheet):
+    def print_character(self, character_sheet):
         print(character_sheet["race"])
         print(character_sheet["name"]) 
 
 if __name__ == "__main__":
     testing_for_loop = for_loop()
-    testing_for_loop.find_character_sheet(testing_for_loop.find_character_sheet(abspath("synthetic-taint-data/resources"))) 
+    testing_for_loop.print_character(testing_for_loop.find_character_sheet(abspath("tests/synthetic-taint-data/resources"))) 
