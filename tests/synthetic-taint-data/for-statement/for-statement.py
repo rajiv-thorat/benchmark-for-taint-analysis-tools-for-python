4461bf28f55f8fc7bb90ac5b07ef8520dacf85b5
Rajiv Thorat <rajiv.thorat@codeshield.io>
2022-04-26 18:10:18 +0200

    Updated taf for 'function' test

--- tests/synthetic-taint-data/for-statement/for-statement.py
+++ tests/synthetic-taint-data/for-statement/for-statement.py
@@ -8,20 +8,20 @@ class for_loop:
        character_sheet = None
        for file in os.listdir(path):
            complete_path = os.path.join(path, file)
-           if os.path.isfile(complete_path) and file == 'character-sheet.json':
+           if os.path.isfile(complete_path) and file == "character-sheet.json":
                character_file = open(complete_path)
                character_sheet = json.load(character_file)
                character_file.close()
-               if character_sheet != '':
-                   print('The loop executed successfully.')
+               if character_sheet != "":
+                   print("The loop executed successfully.")
                    return character_sheet
        if character_sheet is None:
-            print('There was a problem reading the file.')
+            print("There was a problem reading the file.")
 
     def print_character(self, character_sheet):
-        print(character_sheet['race'])
-        print(character_sheet['name']) 
+        print(character_sheet["race"])
+        print(character_sheet["name"]) 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     testing_for_loop = for_loop()
-    testing_for_loop.print_character(testing_for_loop.find_character_sheet(abspath('tests/synthetic-taint-data/resources'))) 
+    testing_for_loop.print_character(testing_for_loop.find_character_sheet(abspath("tests/synthetic-taint-data/resources"))) 
