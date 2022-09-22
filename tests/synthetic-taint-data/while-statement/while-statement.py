fc34805718795ff5c0d57304310242b7a2a8158c
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-09-22 15:48:36 +0200

    Updating the taints

--- tests/synthetic-taint-data/while-statement/while-statement.py
+++ tests/synthetic-taint-data/while-statement/while-statement.py
@@ -2,19 +2,17 @@ from os.path import abspath
 from random import randint
 import json
 from secrets import randbelow
+import sys
+import helper
 
 class while_loop:
     def randomize_prime(self):
         random_number = randint(1,20)
         max_run_count = 10
+        return_value = ''
         while not self.is_prime(random_number) and max_run_count != 0:
-            print("the number is not prime. reading the character sheet.")
-            character_file = open(abspath("tests/synthetic-taint-data/resources/character-sheet.json"))
-            character_sheet = json.load(character_file)
-            character_file.close()
-            self.print_character(character_sheet)
-            random_number = randint(0, 20)
-            max_run_count = max_run_count - 1
+            return_value = sys.argv[1]
+        return return_value
         print("The randomly generated number was a prime number")
     def is_prime(self, random_number):
         if random_number == 2 or random_number == 3:
@@ -29,4 +27,4 @@ class while_loop:
 
 if __name__ == "__main__":
     while_loop_ = while_loop()
-    while_loop_.randomize_prime()
+    helper.run_cmd(while_loop_.randomize_prime()) 
