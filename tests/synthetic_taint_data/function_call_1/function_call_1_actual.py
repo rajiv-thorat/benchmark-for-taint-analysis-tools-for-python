c0a65c527d242efe3af96078ea7fd9ef494141eb
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-10 17:28:25 +0200

    Update function call test, tool op and metrics

---
+++ tests/synthetic_taint_data/function_call_1/function_call_1_actual.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/function_call")
+def function_call() -> None:
+    command = request.view_args.get('operator')
+    command_1 = function_a(command)
+    command_2 = function_b(command_1)
+    # Sink after function call chaining.
+    eval(command_2)
+
+def function_a(command):
+    return command
+
+def function_b(command):
+    return command
