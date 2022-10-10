15c8bc4ed13dc8b059e8e4f40762fd1fc6a1df26
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-10 21:08:40 +0200

    Remaining exception handling cases, and tool op
    and metrics

---
+++ tests/synthetic_taint_data/exceptions_3/exception_3_actual.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+from numpy import empty
+
+app = Flask(__name__)
+
+@app.route("/exception_route")
+def exeption_route() -> None:
+    empty_list = empty( numpy.sqrt(49), dtype=object)
+    command = ''
+    try:
+        command = request.view_args.get('operator')
+        empty_list[2] == ''
+        if empty_list[2] == 'test value':
+            source = 'problem'
+    except:
+        # The sink is after the exception has been caught.
+        eval(command)
