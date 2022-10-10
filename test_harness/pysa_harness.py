0aa03823346663e520d94dac90fbbac22e535588
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-10 13:45:35 +0200

    Op and metrics for 'with' tests

--- test_harness/pysa_harness.py
+++ test_harness/pysa_harness.py
@@ -6,5 +6,5 @@ class PysaHarness(Harness):
         # pyre init-pysa
         # pyre analyze --no-verify --save-results-to ./pysa-runs
         # time -o /op/non_exec_metrics.txt pyre analyze --no-verify --save-results-to /op 
-        # docker run --rm -it -v /home/rajiv/git/MasterArbeit/submodules/benchmark-for-taint-analysis-tools-for-python/test-harness/pysa_files/pysa_config:/config -v /home/rajiv/git/MasterArbeit/submodules/benchmark-for-taint-analysis-tools-for-python/tests/synthetic_taint_data/if_statement_1:/code -v /home/rajiv/git/MasterArbeit/submodules/benchmark-for-taint-analysis-tools-for-python/test_metadata/outputs_raw/pysa/if_statement_1:/op local/pysa
+        # docker run --rm -it -v /home/rajiv/git/MasterArbeit/submodules/benchmark-for-taint-analysis-tools-for-python/test_harness/pysa_files/pysa_config:/config -v /home/rajiv/git/MasterArbeit/submodules/benchmark-for-taint-analysis-tools-for-python/tests/synthetic_taint_data/with_statement_1:/code -v /home/rajiv/git/MasterArbeit/submodules/benchmark-for-taint-analysis-tools-for-python/test_metadata/outputs_raw/pysa/with_statement_1:/op local/pysa
         print('In the pysa harnss')
