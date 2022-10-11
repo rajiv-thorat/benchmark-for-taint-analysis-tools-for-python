22404e467e1b3ce7ca80dce9af047fbaf05774cc
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-11 14:06:56 +0200

    Updated list access test, tool op and metrics

---
+++ tests/synthetic_taint_data/list_access_1/list_access_1_actual.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/list_route")
+def list_route() -> None:
+    command = request.view_args.get('operator')
+    linked_list = collections.deque()
+    linked_list.append(command)
+    linked_list.append('list')
+    linked_list.append('stats')
+    eval(linked_list.popleft())
