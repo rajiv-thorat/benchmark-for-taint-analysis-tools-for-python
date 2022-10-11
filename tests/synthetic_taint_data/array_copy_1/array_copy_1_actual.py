8735a328fad737a942d35200b81c479daa3093e7
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-11 09:22:42 +0200

    Updated array copy test, tool op, and metrics

---
+++ tests/synthetic_taint_data/array_copy_1/array_copy_1_actual.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/array_copy_route")
+def array_copy_route() -> None:
+    command = request.view_args.get('operator')
+    array_1 = ['element 1', command]
+    array_2 = ['element 3', 'element 4', array_1[1]]
+
+    eval(array_2[2])
