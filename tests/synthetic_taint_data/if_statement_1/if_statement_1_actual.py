fc09d5644c7299f07c44520c1ff398fff25bc403
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-14 18:02:06 +0200

    Fixed if test case

--- tests/synthetic_taint_data/if_statement_1/if_statement_1_actual.py
+++ tests/synthetic_taint_data/if_statement_1/if_statement_1_actual.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 from flask import Flask, request
+from random import randint
 
 app = Flask(__name__)
 
 @app.route("/if_route")
 def if_route() -> None:
     command = request.view_args.get('operator')
-    i = 10
+    i = randint(1, 0)
     if i > 0:
         # The sink is inside a branch
         eval(command)
