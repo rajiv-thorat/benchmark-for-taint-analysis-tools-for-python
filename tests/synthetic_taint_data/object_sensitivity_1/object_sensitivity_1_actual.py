3900dd7b567cbabd58c416f25c0a844e06d26025
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-19 17:29:30 +0200

    Update to object sensitivity test

--- tests/synthetic_taint_data/object_sensitivity_1/object_sensitivity_1_actual.py
+++ tests/synthetic_taint_data/object_sensitivity_1/object_sensitivity_1_actual.py
@@ -4,8 +4,8 @@ from class_a import ClassA
 
 app = Flask(__name__)
 
-@app.route("/if_route")
-def if_route() -> None:
+@app.route("/object_route")
+def object_route() -> None:
     command = request.view_args.get('operator')
     instance_1 = ClassA()
     instance_2 = ClassA()
