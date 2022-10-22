2dfa65572f17a02f517e39be8b4c6df113b73d0d
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-22 10:00:26 +0200

    False positive for multi dementional list test case

---
+++ tests/synthetic_taint_data/multi_dementional_array_1/multi_dementional_array_1_false_positive.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+import numpy
+
+app = Flask(__name__)
+
+@app.route("/multi_array_route")
+def multi_array_route() -> None:
+    command = request.view_args.get('operator')
+    multi_dem = numpy.array([command, 'element 1'],['element 2', 'element 3'])
+    source_item = multi_dem[0,1]
+    eval(source_item)
