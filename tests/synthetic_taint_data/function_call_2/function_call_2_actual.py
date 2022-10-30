5c863a4f68e9eab9a19f85fc12ec6f3827f84580
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-10-30 14:22:49 +0100

    Created new test case for function call

---
+++ tests/synthetic_taint_data/function_call_2/function_call_2_actual.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/function_call")
+def function_call() -> None:
+    command = request.view_args.get('operator')
+    function_a(command)
+
+def function_a(command):
+    eval(command)
