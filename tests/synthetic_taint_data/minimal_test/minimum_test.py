416b56d4caede40fb163f79ff5ddcf7768bf2a01
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-11-01 09:00:01 +0100

    Updated the minimal tests

--- tests/synthetic_taint_data/minimal_test/minimum_test.py
+++
@@ -1,5 +0,0 @@
-import sys
-from subprocess import check_call
-
-variable = sys.argv[1]
-check_call(['ls', variable])
---
+++ tests/synthetic_taint_data/minimal_test/minimum_test_2_actual.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/minimal_route")
+def minimal_route() -> None:
+    command = request.view_args.get('command')
+    char = Character(command)
+
+class Character:
+    def __init__(self, command):
+        eval(command)
