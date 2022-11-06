18f0aa7a6e00fc6535b8d1f386617e8024a6495b
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-11-06 13:19:44 +0100

    Restored the if test case

--- tests/synthetic_tests/if_statement_1/if_statement_1_actual.py
+++ tests/synthetic_tests/if_statement_1/if_statement_1_actual.py
@@ -6,15 +6,12 @@ This test checks whether the analyses performed by the tools can handle branchin
 Since the execution of the path to the sink can only be decided at runtime, the analyses must include the branch, i.e., detect a taint flow.
 """
 from flask import Flask, request
-import subprocess
 
 app = Flask(__name__)
 
 @app.route("/if_route")
 def if_route() -> None:
     command = request.view_args.get('command')
-    param = request.form['command']
     if True:
         # The sink is inside a branch
         eval(command)
-        subprocess.call(param)
