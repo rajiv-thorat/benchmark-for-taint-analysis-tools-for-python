6a4ac2a371c49153fc98d262291d6c81537c10a5
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-11-06 17:08:36 +0100

    Finalized the main method

--- test_harness/main.py
+++ test_harness/main.py
@@ -6,10 +6,6 @@ from os import path
 import logging
 
 if __name__== '__main__':
-    """ if not utils.DIRECTORY_FOR_RAW_OUTPUT.exists():
-        os.mkdir(utils.DIRECTORY_FOR_RAW_OUTPUT)
-    if not utils.DIRECTORY_FOR_EXTRACTED_OUTPUT.exists():
-        os.mkdir(utils.DIRECTORY_FOR_EXTRACTED_OUTPUT) """
     for tool in Harness.__subclasses__():
         tool_harness_instance = tool()
         for test_directory in [x for x in utils.DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA.iterdir() if x.is_dir() and x.name != 'experiments']:
@@ -17,6 +13,7 @@ if __name__== '__main__':
             tool_harness_instance.make_output_directories(test_directory)
             tool_harness_instance.run_tool_on_directory(test_directory)
             tool_harness_instance.move_results(test_directory)
+            tool_harness_instance.record_results(test_directory)
     
     coll = NonExecMetricsCollector()
     coll.collect()
