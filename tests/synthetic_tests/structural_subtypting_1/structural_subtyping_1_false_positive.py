24708b23fa4191c989cf05e32a0020699ffa476c
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-11-06 23:01:52 +0100

    Finished the duck typing test

---
+++ tests/synthetic_tests/structural_subtypting_1/structural_subtyping_1_false_positive.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""  
+This test case check if the tools can work with duck typing. The sink is in class ClassB. The function sink is called from the function evaluate.
+Which one of the classes is used depends on the instance passed to the evaluate function. In this case, the tainted data is passed to the evaluate function,
+then to the Proto Class, and ends with the sink function from the class ClassB.
+"""
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+class Proto(Protocol):
+    def sink(self, command):
+        ...
+
+def evaluate(instance:Proto, command):
+    instance.sink(command)
+
+class ClassA:
+    def sink(self, command):
+        eval('list')
+
+class ClassB:
+    def sink(self, command):
+        eval(command)
+
+@app.route("/duck_route")
+def duck_route() -> None:
+    command = request.view_args.get('command')
+    evaluate(ClassA(), command)
