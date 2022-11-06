d68a9344befeb4731b28868b4bfb66e21c8a225e
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-11-06 22:10:31 +0100

    Added the decorator test cases

---
+++ tests/synthetic_tests/decorator_1/decorator_1_actual.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+""" 
+In this test case we check the support for decorators. The method decorator_route() contains the source and is wrapped in the tainted_decorator.
+The sink is in the tainted_decorator. This decorator is is the function type.
+"""
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@tainted_decorator
+@app.route("/decorator_route")
+def decorator_route() -> None:
+    command = request.view_args.get('command')
+    return command
+
+def tainted_decorator(funct):
+    command = func(*args, **kwargs)
+    eval(command)
---
+++ tests/synthetic_tests/decorator_2/decorator_2_actual.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""  
+In this test case we check the support for decorators. The method decorator_route() contains the source and is wrapped in the TaintedDecorator.
+The sink is in the __call__ function of class TaintedDecorator. This decorator is the class type.
+"""
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@TaintedDecorator
+@app.route("/decorator_route")
+def decorator_route() -> None:
+    command = request.view_args.get('command')
+    return command
+
+class TaintedDecorator:
+    def __init__(self, func):
+        self.function = func
+
+    def __call__(self, *args, **kwargs):
+        command = self.function(*args, **kwargs)
+        eval(command)
