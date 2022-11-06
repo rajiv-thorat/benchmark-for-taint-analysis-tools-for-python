06e2e6d97636f0654665e93cd74105828f216617
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-11-06 15:03:41 +0100

    Finished the PyT harness

--- test_harness/pyt_harness.py
+++ test_harness/pyt_harness.py
@@ -2,7 +2,10 @@ from harness import Harness
 import logging
 from pathlib import Path
 import utils
+from subprocess import run
+from shutil import copy
 
+logging.basicConfig(level=logging.DEBUG)
 class PytHarness(Harness):
     harness_name = 'pyt'
     def run_tool_on_directory(self, test_input_directory:Path):
@@ -16,23 +19,25 @@ class PytHarness(Harness):
         '-v', f'{utils.DIRECTORY_FOR_THE_PYT_PROJECT.absolute().__str__()}:/pyt',
         '-v', f'{self.get_raw_output_dir_for_input_dir(test_input_directory).absolute().__str__()}:/op', 
         'local/pyt-container']
-        command_output = subprocess.run(command)
+        command_output = run(command)
         logging.info(f'Finished executing {command_output.args}')
         try:
             command_output.check_returncode()
         except:
-            logging.error(f"The subprocess returned {command_output.returncode} code. There was a problem running the Pysa docker image.")
+            logging.error(f"The subprocess returned {command_output.returncode} code. Pyt returns an error code when issues are found.")
 
     def move_results(self, test_input_directory):
+        logging.info(f'Caching the results at {self.get_raw_output_dir_for_input_dir(test_input_directory).__str__()}.')
         copy(self.get_raw_output_dir_for_input_dir(test_input_directory).joinpath('vuls.json').absolute().__str__(), 
         self.get_ext_output_dir_for_input_dir(test_input_directory).absolute().__str__())
         copy(self.get_raw_output_dir_for_input_dir(test_input_directory).joinpath('non_exec_metrics.txt').absolute().__str__(), 
         self.get_ext_output_dir_for_input_dir(test_input_directory).absolute().__str__())
 
     def record_results(self, test_input_directory):
+        logging.info('Recording the results.')
         files_to_look_for = self.get_test_files_for_result_evaluation(test_directory)
-        #output_data = utils.read_json_file(self.get_ext_output_dir_for_input_dir(test_input_directory).joinpath('vuls.json'))
-        output_data = utils.read_json_file(Path('/home/rajiv/temp_/vuls.json'))
+        output_data = utils.read_json_file(self.get_ext_output_dir_for_input_dir(test_input_directory).joinpath('vuls.json'))
+        #output_data = utils.read_json_file(Path('/home/rajiv/temp_/vuls.json'))
         results = {}
         for file_to_look_for in files_to_look_for:
             results[file_to_look_for] = False
@@ -49,7 +54,7 @@ if __name__== '__main__':
     tool_harness_instance = PytHarness()
     test_directory = Path('tests/synthetic_tests/if_statement_1')
     logging.info(f'Running {tool_harness_instance.get_harness_type()} on test {test_directory.name}.')
-    """ tool_harness_instance.make_output_directories(test_directory)
+    tool_harness_instance.make_output_directories(test_directory)
     tool_harness_instance.run_tool_on_directory(test_directory)
-    tool_harness_instance.move_results(test_directory) """
+    tool_harness_instance.move_results(test_directory)
     tool_harness_instance.record_results(test_directory)
