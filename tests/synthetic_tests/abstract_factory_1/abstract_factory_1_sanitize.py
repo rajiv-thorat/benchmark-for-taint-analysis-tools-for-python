7f3e6ef1659691cf198915c1e8c252de2e7db058
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-11-06 20:10:32 +0100

    Finalizing the abstract factory test

--- tests/synthetic_tests/abstract_factory_1/abstract_factory_1_sanitize.py
+++ tests/synthetic_tests/abstract_factory_1/abstract_factory_1_sanitize.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
+""" 
+The abstract factory design pattern is a way to encapsulate a group of objects with a common
+theme without the concrete class. In this test case , the
+objects are initialized at runtime and returned by the function factory(). The get_command() 
+function in ClassA returns the command field as it is set, i.e., the taint is propagated. The
+get_command() function of ClassB, on the other hand, returns a constant string, i.e., breaking
+any taint flow. Which object is created at runtime will be difficult for static analysis to determine.
+This test includes sanitization.
+"""
 from flask import Flask, request
 from class_a import ClassA
 from class_b import ClassB
@@ -14,7 +23,7 @@ def factory(class_name: str):
 
 @app.route("/factory_route")
 def factory_route() -> None:
-    command = request.view_args.get('operator')
+    command = request.view_args.get('command')
     a = factory("ClassA")
     a.set_command(command)
     eval(a.get_command_sanitized())
