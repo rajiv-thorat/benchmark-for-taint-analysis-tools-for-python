554dbf4554751e7483ec243891935f353fda48e4
Rajiv Thorat <rajivthorat2003@gmail.com>
2022-11-08 17:54:26 +0100

    Small fixes to the harness

--- test_harness/main.py
+++ test_harness/main.py
@@ -8,10 +8,10 @@ from non_exec_metrics_collector import NonExecMetricsCollector
 from validator import Validator
 
 if __name__== '__main__':
-    Validator.do_validations()
+    #Validator.do_validations()
     for tool in Harness.__subclasses__():
         tool_harness_instance = tool()
-        for test_directory in [x for x in utils.DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA.iterdir() if x.is_dir() and x.name != 'experiments']:
+        for test_directory in [x for x in utils.DIRECTORY_PATH_FOR_SYNTHETIC_TAINT_DATA.iterdir() if x.is_dir() and (x.name != 'experiments' or x.name != 'match_statement_1')]:
             logging.info(f'Running {tool_harness_instance.get_harness_type()} on test {test_directory.name}.')
             tool_harness_instance.make_output_directories(test_directory)
             tool_harness_instance.run_tool_on_directory(test_directory)
